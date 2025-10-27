95a4901a07809e0360f0994f8066dd8908c42f48
prashantgohel321 <smaterial123321@gmail.com>
2025-10-27 20:35:09 +0530

    Python Day 01 Task 01 Swap Values of two variables

---
+++ Python For DevOps/Programs/Others/05 Oct 2025/005.py
@@ -0,0 +1,7 @@
+"""
+Additional 05: 
+Write a program which takes input value for age from user and display message if user is eligible for vote.    
+"""
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible for vote.")
